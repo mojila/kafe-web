aab9ee930e021c2756ff4b56b459d1f05e9ef222
Mojila <me.mojila@gmail.com>
2019-12-24 13:11:55 +0700

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KafeWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
